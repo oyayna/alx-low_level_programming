cf555c09b6829fbfda8e81c28e917ff7f600dbaa
melio <melio@kali>
2023-09-27 00:30:46 +0100

    Update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,77 +1,65 @@
-#include "lists.h"
+include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * free_listp2 - Frees a list of pointers to structures and sets head to NULL.
- * @head: Pointer to the pointer to the list's head.
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
  *
- * Description:
- * This function deallocates memory for a list of pointers to structures
- * and sets the head pointer to NULL.
+ * Return: pointer to the new list
  */
-void free_listp2(listp_t **head)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-listp_t *temp;
-listp_t *curr;
+    listint_t **newlist;
+    size_t i;
 
-if (head != NULL)
-{
-curr = *head;
-while ((temp = curr) != NULL)
-{
-curr = curr->next;
-free(temp);
-}
-*head = NULL;
-}
+    newlist = malloc(size * sizeof(listint_t *));
+    if (newlist == NULL)
+    {
+        free(list);
+        exit(98);
+    }
+    for (i = 0; i < size - 1; i++)
+        newlist[i] = list[i];
+    newlist[i] = new;
+    free(list);
+    return (newlist);
 }
 
 /**
- * free_listint_safe - Safely frees a listint_t list (handles loops).
- * @h: Pointer to the pointer to the list's head.
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
  *
- * Description:
- * This function deallocates memory for a listint_t list, loops if present.
- *
- * Return: Number of nodes freed, or 0 if a loop is detected.
+ * Return: the number of nodes in the list
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
-size_t nnodes = 0;
-listp_t *hptr, *new, *add;
-listint_t *curr;
+    size_t i, num = 0;
+    listint_t **list = NULL;
+    listint_t *next;
 
-hptr = NULL;
-while (*h != NULL)
-{
-new = malloc(sizeof(listp_t));
-
-if (new == NULL)
-exit(98);
-
-new->p = (void *)*h;
-new->next = hptr;
-hptr = new;
-
-add = hptr;
-
-while (add->next != NULL)
-{
-add = add->next;
-if (*h == add->p)
-{
-*h = NULL;
-free_listp2(&hptr);
-return (nnodes);
-}
-}
-
-curr = *h;
-*h = (*h)->next;
-free(curr);
-nnodes++;
-}
-
-*h = NULL;
-free_listp2(&hptr);
-return (nnodes);
+    if (head == NULL || *head == NULL)
+        return (num);
+    while (*head != NULL)
+    {
+        for (i = 0; i < num; i++)
+        {
+            if (*head == list[i])
+            {
+                *head = NULL;
+                free(list);
+                return (num);
+            }
+        }
+        num++;
+        list = _ra(list, num, *head);
+        next = (*head)->next;
+        free(*head);
+        *head = next;
+    }
+    free(list);
+    return (num);
 }
