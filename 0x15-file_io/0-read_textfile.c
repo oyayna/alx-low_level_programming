6f97bef1e15285e47dd0b18f9e231fd9913c97de
melio <melio@kali>
2023-10-03 02:41:36 +0100

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
+	buffer = calloc(letters , sizeof(char));
 	if (buffer == NULL)
 		return (0);
 
